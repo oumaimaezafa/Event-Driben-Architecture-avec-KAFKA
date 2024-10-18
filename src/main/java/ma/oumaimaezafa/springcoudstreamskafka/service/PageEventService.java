44f6a661f9d972b27bc68f34e88011ce35ddc1f0
Oumaima Ez <oumaimaezafa12@gmail.com>
2024-10-18 23:48:29 +0100

    case 5 kafka streams

--- src/main/java/ma/oumaimaezafa/springcoudstreamskafka/service/PageEventService.java
+++ src/main/java/ma/oumaimaezafa/springcoudstreamskafka/service/PageEventService.java
@@ -2,9 +2,17 @@ package ma.oumaimaezafa.springcoudstreamskafka.service;
 
 
 import ma.oumaimaezafa.springcoudstreamskafka.enteties.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Time;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -44,5 +52,19 @@ public class PageEventService {
     }
 
     //5 eme case
+    @Bean
+    public Function<KStream<String,PageEvent>,KStream<String,Long>>kStreamFunction(){
+        return (input)->{
+          return input
+                  .filter((k,v)->v.getDuration()>300)
+                  .map((k,v)->new KeyValue<>(v.getName(),0L))
+                  .groupBy((k,v)->k, Grouped.with(Serdes.String(),Serdes.Long()))
+                   //sur l'ensemble des enrg qui ont ete observes sans les le derniers 5 s
+                  .windowedBy(TimeWindows.of(Duration.ofMillis(5000)))
+                  .count(Materialized.as("page-count"))
+                  .toStream()
+                  .map((k,v)->new KeyValue<>("=>"+k.window().startTime()+k.window().endTime()+k.key(),v));
+            };
+        }
+    }
 
-}
